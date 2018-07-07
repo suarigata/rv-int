22702591f4d86202e12b3120c85ed938a2b0a842
Alex Lucchesi <alexpdinfo@gmail.com>
2018-07-07 03:43:02 -0300

    Decoder Completed

--- RVDecoder.cpp
+++ RVDecoder.cpp
@@ -73,46 +73,46 @@ EncodingType dbt::RVDecoder::getEncodingType(RVInstType InstType) {
     case RVInstType::DIVU:
     case RVInstType::REM:
     case RVInstType::REMU:
-    case RVInstType::FADD.S:
-    case RVInstType::FSUB.S:
-    case RVInstType::FMUL.S:
-    case RVInstType::FDIV.S:
-    case RVInstType::FSQRT.S:
-    case RVInstType::FSGNJ.S:
-    case RVInstType::FSGNJN.S:
-    case RVInstType::FSGNJX.S:
-    case RVInstType::FMIN.S:
-    case RVInstType::FMAX.S:
-    case RVInstType::FCVT.W.S:
-    case RVInstType::FCVT.WU.S:
-    case RVInstType::FMV.X.W:
-    case RVInstType::FEQ.S:
-    case RVInstType::FLT.S:
-    case RVInstType::FLE.S:
-    case RVInstType::FCLASS.S:
-    case RVInstType::FCVT.S.W:
-    case RVInstType::FCVT.S.WU:
-    case RVInstType::FMV.W.X:
-    case RVInstType::FADD.D:
-    case RVInstType::FSUB.D:
-    case RVInstType::FMUL.D:
-    case RVInstType::FDIV.D:
-    case RVInstType::FSQRT.D:
-    case RVInstType::FSGNJ.D:
-    case RVInstType::FSGNJN.D:
-    case RVInstType::FSGNJX.D:
-    case RVInstType::FMIN.D:
-    case RVInstType::FMAX.D:
-    case RVInstType::FCVT.S.D:
-    case RVInstType::FCVT.D.S:
-    case RVInstType::FEQ.D:
-    case RVInstType::FLT.D:
-    case RVInstType::FLE.D:
-    case RVInstType::FCLASS.D:
-    case RVInstType::FCVT.W.D:
-    case RVInstType::FCVT.WU.D:
-    case RVInstType::FCVT.D.W:
-    case RVInstType::FCVT.D.WU:
+    case RVInstType::FADD_S:
+    case RVInstType::FSUB_S:
+    case RVInstType::FMUL_S:
+    case RVInstType::FDIV_S:
+    case RVInstType::FSQRT_S:
+    case RVInstType::FSGNJ_S:
+    case RVInstType::FSGNJN_S:
+    case RVInstType::FSGNJX_S:
+    case RVInstType::FMIN_S:
+    case RVInstType::FMAX_S:
+    case RVInstType::FCVT_W_S:
+    case RVInstType::FCVT_WU_S:
+    case RVInstType::FMV_X_W:
+    case RVInstType::FEQ_S:
+    case RVInstType::FLT_S:
+    case RVInstType::FLE_S:
+    case RVInstType::FCLASS_S:
+    case RVInstType::FCVT_S_W:
+    case RVInstType::FCVT_S_WU:
+    case RVInstType::FMV_W_X:
+    case RVInstType::FADD_D:
+    case RVInstType::FSUB_D:
+    case RVInstType::FMUL_D:
+    case RVInstType::FDIV_D:
+    case RVInstType::FSQRT_D:
+    case RVInstType::FSGNJ_D:
+    case RVInstType::FSGNJN_D:
+    case RVInstType::FSGNJX_D:
+    case RVInstType::FMIN_D:
+    case RVInstType::FMAX_D:
+    case RVInstType::FCVT_S_D:
+    case RVInstType::FCVT_D_S:
+    case RVInstType::FEQ_D:
+    case RVInstType::FLT_D:
+    case RVInstType::FLE_D:
+    case RVInstType::FCLASS_D:
+    case RVInstType::FCVT_W_D:
+    case RVInstType::FCVT_WU_D:
+    case RVInstType::FCVT_D_W:
+    case RVInstType::FCVT_D_WU:
       return EncodingType::ER;
     case RVInstType::JALR:
     case RVInstType::LB:
@@ -127,7 +127,7 @@ EncodingType dbt::RVDecoder::getEncodingType(RVInstType InstType) {
     case RVInstType::ORI:
     case RVInstType::ANDI:
     case RVInstType::FENCE:
-    case RVInstType::FENCE.I:
+    case RVInstType::FENCE_I:
     case RVInstType::ECALL:
     case RVInstType::EBREAK:
     case RVInstType::CSRRW:
@@ -157,14 +157,14 @@ EncodingType dbt::RVDecoder::getEncodingType(RVInstType InstType) {
       return EncodingType::EU;
     case RVInstType::JAL:
       return EncodingType::EJ;
-    case RVInstType::FMADD.S:
-    case RVInstType::FMSUB.S:
-    case RVInstType::FNMSUB.S:
-    case RVInstType::FNMADD.S:
-    case RVInstType::FMADD.D:
-    case RVInstType::FMSUB.D:
-    case RVInstType::FNMSUB.D:
-    case RVInstType::FNMADD.D:
+    case RVInstType::FMADD_S:
+    case RVInstType::FMSUB_S:
+    case RVInstType::FNMSUB_S:
+    case RVInstType::FNMADD_S:
+    case RVInstType::FMADD_D:
+    case RVInstType::FMSUB_D:
+    case RVInstType::FNMSUB_D:
+    case RVInstType::FNMADD_D:
       return EncodingType::ER4;
   }
 
@@ -269,11 +269,7 @@ RVInst dbt::RVDecoder::decode(uint32_t CodedInst) {
         case 0b111: I.Type = RVInstType::ANDI;	break; // ANDI
         case 0b001: I.Type = RVInstType::SLLI;	break; // SLLI
         case 0b101: // SRLI | SRAI
-          if(getF7(W))
-            I.Type = RVInstType::SRAI;
-          else
-            I.Type = RVInstType::SRLI;
-          break;
+          I.Type = (getF7(W) ? RVInstType::SRAI : RVInstType::SRLI); break;
       }
       break;
     case 0b0110011:
@@ -291,21 +287,13 @@ RVInst dbt::RVDecoder::decode(uint32_t CodedInst) {
       else
         switch(getF3(W)){
           case 0b000: // ADD | SUB
-            if(getF7(W))
-              I.Type = RVInstType::SUB;
-            else
-              I.Type = RVInstType::ADD;
-            break;
+            I.Type = (getF7(W) ? RVInstType::SUB : RVInstType::ADD); break;
           case 0b001: I.Type = RVInstType::SLL;		break; // SLL
           case 0b010: I.Type = RVInstType::SLT;		break; // SLT
           case 0b011: I.Type = RVInstType::SLTU;	break; // SLTU
           case 0b100: I.Type = RVInstType::XOR;		break; // XOR
           case 0b101: // SRL | SRA
-            if(getF7(W))
-              I.Type = RVInstType::SRA;
-            else
-              I.Type = RVInstType::SRL;
-            break;
+            I.Type = (getF7(W) ? RVInstType::SRA : RVInstType::SRL); break;
           case 0b110: I.Type = RVInstType::OR;		break; // OR
           case 0b111: I.Type = RVInstType::AND;		break; // AND
         }
@@ -314,13 +302,9 @@ RVInst dbt::RVDecoder::decode(uint32_t CodedInst) {
       I.Type = RVInstType::ADD; // TODO
       break;
     case 0b1110011: // SYSTEM
-      switch(getF3(F)){
+      switch(getF3(W)){
         case 0b000: // ECALL | EBREAK
-          if(getImmI(W))
-            I.Type = RVInstType::EBREAK;
-          else
-            I.Type = RVInstType::ECALL;
-          break;
+          I.Type = ((getImmI(W)) ? RVInstType::EBREAK : RVInstType::ECALL); break;
         case 0b001: I.Type = RVInstType::CSRRW;		break; // CSRRW
         case 0b010: I.Type = RVInstType::CSRRS;		break; // CSRRS
         case 0b011: I.Type = RVInstType::CSRRC;		break; // CSRRC
@@ -329,7 +313,79 @@ RVInst dbt::RVDecoder::decode(uint32_t CodedInst) {
         case 0b111: I.Type = RVInstType::CSRRCI;	break; // CSRRCI
       }
       break;
-
+    case 0b0000111:
+      I.Type = ((getF3(W) & 0x1) ? RVInstType::FLD : RVInstType::FLW); break;
+    case 0b0100111:
+      I.Type = ((getF3(W) & 0x1) ? RVInstType::FSD : RVInstType::FSW); break;
+    case 0b1000011:
+      I.Type = ((getF2(W)) ? RVInstType::FMADD_D : RVInstType::FMADD_F); break;
+    case 0b1000111:
+      I.Type = ((getF2(W)) ? RVInstType::FMSUB_D : RVInstType::FMSUB_F); break;
+    case 0b1001011:
+      I.Type = ((getF2(W)) ? RVInstType::FNMSUB_D : RVInstType::FNMSUB_F); break;
+    case 0b1001111:
+      I.Type = ((getF2(W)) ? RVInstType::FNMADD_D : RVInstType::FNMADD_F); break;
+    case 0b1010011:
+      switch(getF7(W)){
+        case 0b0000000: I.Type = RVInstType::FADD_S;	break; // FADD_S
+        case 0b0000001: I.Type = RVInstType::FADD_D;	break; // FADD_D
+        case 0b0000100: I.Type = RVInstType::FSUB_S;	break; // FSUB_S
+        case 0b0000101: I.Type = RVInstType::FSUB_D;	break; // FSUB_D
+        case 0b0001000: I.Type = RVInstType::FMUL_S;	break; // FMUL_S
+        case 0b0001001: I.Type = RVInstType::FMUL_D;	break; // FMUL_D
+        case 0b0001100: I.Type = RVInstType::FDIV_S;	break; // FDIV_S
+        case 0b0001101: I.Type = RVInstType::FDIV_D;	break; // FDIV_D
+        case 0b0010000:
+          switch(getF3(W)){
+            case 0b000: I.Type = RVInstType::FSGNJ_S;	break; // FSGNJ_S
+            case 0b001: I.Type = RVInstType::FSGNJN_S;	break; // FSGNJN_S
+            case 0b010: I.Type = RVInstType::FSGNJX_S;	break; // FSGNJX_S
+          }
+          break;
+        case 0b0010001:
+          switch(getF3(W)){
+            case 0b000: I.Type = RVInstType::FSGNJ_D;	break; // FSGNJ_D
+            case 0b001: I.Type = RVInstType::FSGNJN_D;	break; // FSGNJN_D
+            case 0b010: I.Type = RVInstType::FSGNJX_D;	break; // FSGNJX_D
+          }
+          break;
+        case 0b0010100:
+          I.Type = (getF3(W) ? RVInstType::FMAX_S : RVInstType::FMIN_S); break;
+        case 0b0010101:
+          I.Type = (getF3(W) ? RVInstType::FMAX_D : RVInstType::FMIN_D); break;
+        case 0b0100000: I.Type = RVInstType::FCVT_S_D;	break; // FCVT_S_D
+        case 0b0100001: I.Type = RVInstType::FCVT_D_S;	break; // FCVT_D_S
+        case 0b0101100: I.Type = RVInstType::FSQRT_S;	break; // FSQRT_S
+        case 0b0101101: I.Type = RVInstType::FSQRT_D;	break; // FSQRT_D
+        case 0b1010000:
+          switch(getF3(W)){
+            case 0b000: I.Type = RVInstType::FLE_S;	break; // FLE_S
+            case 0b001: I.Type = RVInstType::FLT_S;	break; // FLT_S
+            case 0b010: I.Type = RVInstType::FEQ_S;	break; // FEQ_S
+          }
+          break;
+        case 0b1010001:
+          switch(getF3(W)){
+            case 0b000: I.Type = RVInstType::FLE_D;	break; // FLE_D
+            case 0b001: I.Type = RVInstType::FLT_D;	break; // FLT_D
+            case 0b010: I.Type = RVInstType::FEQ_D;	break; // FEQ_D
+          }
+          break;
+        case 0b1100000:
+          I.Type = (getRS2(W) ? RVInstType::FCVT_WU_S : RVInstType::FCVT_W_S); break;
+        case 0b1100001:
+          I.Type = (getRS2(W) ? RVInstType::FCVT_WU_D : RVInstType::FCVT_W_D); break;
+        case 0b1101000:
+          I.Type = (getRS2(W) ? RVInstType::FCVT_S_WU : RVInstType::FCVT_S_W); break;
+        case 0b1101001:
+          I.Type = (getRS2(W) ? RVInstType::FCVT_D_WU : RVInstType::FCVT_D_W); break;
+        case 0b1110000:
+          I.Type = (getF3(W) ? RVInstType::FCLASS_S : RVInstType::FMV_X_W); break;
+        case 0b1110001: I.Type = RVInstType::FCLASS_D;	break; // FCLASS_D
+        case 0b1111000: I.Type = RVInstType::FMV_W_X;	break; // FMV_W_X
+      }
+      break;
+    
     default:
       I.Type = RVInstType::Null;
   }
@@ -348,3 +404,4 @@ RVInst dbt::RVDecoder::decode(uint32_t CodedInst) {
 
   return I;
 }
+
